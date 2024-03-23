04f2638850214ee927d3ddc750f215b1d8c5f993
piszczel24 <s26479@pjwstk.edu.pl>
2024-03-23 21:02:15 +0100

    Modyfikacja 2

--- Program.cs
+++ Program.cs
@@ -7,5 +7,8 @@ internal abstract class Program
         Console.WriteLine("Hello, World!");
 
         for (var i = 0; i < 10; i++) Console.Write($"{i} ");
+
+        int[] arr = { 1, 2, 3, 4, 5 };
+        foreach (var number in arr) Console.WriteLine($"{number * 2} ");
     }
 }

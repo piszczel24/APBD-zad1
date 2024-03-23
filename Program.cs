b00fb26ef2e350f13edf9242592a700d895d018b
piszczel24 <s26479@pjwstk.edu.pl>
2024-03-23 21:24:26 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,10 +6,21 @@ internal abstract class Program
     {
         int[] array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         Console.WriteLine(Average(array));
+        Console.WriteLine(Max(array));
     }
 
     private static float Average(IReadOnlyCollection<int> numbers)
     {
         return (float)numbers.Sum() / numbers.Count;
+    }
+
+    private static float Max(IReadOnlyList<int> numbers)
+    {
+        var max = numbers[0];
+        for (var i = 1; i < numbers.Count; i++)
+            if (numbers[i] > max)
+                max = numbers[i];
+
+        return max;
     }
 }
